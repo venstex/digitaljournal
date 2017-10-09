4ce847ffa54133e8978c8dbcb9ab7bad72f229df
Steven <savdschoot@gmail.com>
2017-10-09 13:19:15 +0200

    eerste versie

---
+++ src/main/java/com/digitaljournal/digitaljournal/DigitaljournalApplication.java
@@ -0,0 +1,12 @@
+package com.digitaljournal.digitaljournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitaljournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitaljournalApplication.class, args);
+	}
+}
